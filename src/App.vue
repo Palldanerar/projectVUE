1c0f2f6ea76e42733fc0414ec196c651bd7a0672
Palldanerar <103989677+Palldanerar@users.noreply.github.com>
2023-03-25 21:30:22 +0300

    v1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1>Hi world!</h1>
+  <h1>{{ title }}</h1>
 </template>
 
-<style scoped>
-</style>
+<script>
+export default {
+  data() {
+    return {
+      title: "Hi world!",
+    };
+  },
+  methods: {
+    get() {
+      fetch("https://api.adviceslip.com/advice")
+        .then((res) => res.json())
+        .then((data) => (this.title = data.slip.advice));
+    },
+  },
+  mounted() {
+    this.get();
+  },
+};
+</script>
+
+<style scoped></style>
